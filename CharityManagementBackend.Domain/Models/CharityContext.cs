2ecaad1388fa706d8de03d9676a1ed6b5b81dff3
prhmkm <119725291+prhmkm@users.noreply.github.com>
2023-07-24 10:47:05 +0330

    Update CharityContext.cs

--- CharityManagementBackend.Domain/Models/CharityContext.cs
+++ CharityManagementBackend.Domain/Models/CharityContext.cs
@@ -31,7 +31,7 @@ namespace CharityManagementBackend.Domain.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("data source=185.255.88.106,2019; initial catalog=charity;User Id=Vira;Password=P@3w0rd!;");
+                o
             }
         }
 
